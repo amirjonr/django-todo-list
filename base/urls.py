7970cc159c96e0f47170e09326224b48911bb933
amirjonr <amirshorakhimov0017@gmail.com>
2024-05-23 16:09:19 +0500

    ended crud app

--- base/urls.py
+++ base/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
-from .views import TaskList, TaskDetail
+from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete
 
 urlpatterns = [
     path('', TaskList.as_view(), name='tasks'),
     path('tasks/<int:pk>/', TaskDetail.as_view(), name='task'),
+    path('tasks/create', TaskCreate.as_view(), name='task-create'),
+    path('tasks/<int:pk>/update', TaskUpdate.as_view(), name='task-update'),
+    path('tasks/<int:pk>/delete', TaskDelete.as_view(), name='task-delete'),
 ]
